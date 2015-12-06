952f0cdd936f974d222fe33c891a7c124e2635b7
cwen <cwen@regenstrief.org>
2015-12-05 20:39:49 -0500

    Apply same viewmodel to another.html, show index page's input value.

--- web/js/appViewModel.js
+++ web/js/appViewModel.js
@@ -1,12 +1,39 @@
 // Main viewmodel class
-define(['ko', 'anotherViewModel'], function(ko, anotherViewModel) {
+define(['ko', 'anotherViewModel', 'jquery'], function(ko, anotherViewModel, $) {
     return function appViewModel() {
+    	self.model = this;
         this.inputLetters = ko.observable('try this');
         this.inputCaps = ko.pureComputed(function() {
             return this.inputLetters().toUpperCase();
         }, this);
         this.redirect = function () {
-            return new anotherViewModel(this.inputCaps);
+            //return new anotherViewModel(this.inputCaps);
+        	//$('body').load("another.html", function () {
+        		//ko.cleanNode($('body'));
+        		//ko.applyBindings(new anotherViewModel(), $("body"));
+        		//ko.applyBindings(new anotherViewModel(), $('body'));
+        		//ko.applyBindings(new anotherViewModel());
+        	//});
+//        	 $.ajax({
+//                 type: "POST",
+//                 url: "another.html",
+//                 data: { inputCaps: this.inputCaps },
+//                 success: function (data) {
+                	 //alert(data);
+//                	 $("body").html(data);
+//                	 ko.applyBindings(new anotherViewModel(), $("body"));
+//                 },
+//                 error: function () {
+//                 }
+//             });
+            $.ajax({
+				type: 'GET',
+				url: 'another.html', 
+				success: function (data) {
+	                $('#indexDiv').html(data);
+	                ko.applyBindings(self.model, $('#anotherDiv')[0]);
+				}
+			});
         };
     };
 });
--- web/js/main.js
+++ web/js/main.js
@@ -2,8 +2,10 @@ requirejs.config({
     baseUrl: 'js',
 	paths: {
         "ko": "lib/knockout-3.4.0",
+        //"jquery": "lib/jquery-1.11.3.min.js",
+        "jquery":  "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min",
         "appViewModel": "appViewModel",
-        "anotherViewModel": "anotherViewModel"
+        "anotherViewModel": "anotherViewModel",
 	}
 });
 
